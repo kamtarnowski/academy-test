23a26bcb050564c67f26aeaac0874bf07606d31e
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-02-19 20:12:53 +0100

    Added 16 cities to seed file with appropriate short_names.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ City.destroy_all
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
+
 City.create(name: 'Cracow', short_name: 'Cr')
 
 num = 1
@@ -15,4 +16,11 @@ num = 1
   password = 'password'
   User.create!(name: name, email: email, age: age, city_id: city_id, password: password, password_confirmation: password)
   num += 1
+end
+
+16.times do
+  name = Faker::Address.city
+  splited_name = name.split('')
+  sh_name = splited_name.first + splited_name.last
+  City.create(name: name, short_name: sh_name.capitalize)
 end
